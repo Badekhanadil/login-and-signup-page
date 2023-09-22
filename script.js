1a9fd8b6d518fdb6f7e5c80c8b2cb403747a8944
badekhanadil <badekhanadil5@gmail.com>
2023-09-22 19:24:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Login Form
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // You can add your login validation logic here
+
+    // For demonstration, let's simply redirect to a welcome page
+    window.location.href = 'welcome.html';
+});
+
+// Signup Form
+document.getElementById('signup-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // You can add your signup validation logic here
+
+    // For demonstration, let's simply redirect to a welcome page
+    window.location.href = 'welcome.html';
+});
